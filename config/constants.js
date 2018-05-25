8bc3e935acd26b225ea38e818b4627827a22ecac
Casey Childers <cachilders@gmail.com>
2018-05-25 05:58:58 -0700

    update timeout and change name

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
   allKeys: ['assignee', 'created', 'description', 'id', 'issue_type', 'status', 'summary', 'ticket', 'updated'],
   persistentKeys: ['created', 'id', 'ticket'],
-  timeout: 60 * 1000,
+  timeout: 5 * 60 * 1000,
 };
